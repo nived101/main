01512159fb1cbf4b63a90e634b5657be6fd9377c
nived <nivedkm101@gmail.com>
2025-02-14 14:39:18 +0530

    comit

--- views.py
+++ views.py
@@ -182,6 +182,140 @@ def home(request):
         {"title": "NCC", "link": "/students/studentActivities/NCC"},
         {"title": "Support Center", "url": "http://172.18.5.212/"},
     ]
+    eventsCarouselSlides = [
+        {"title": "Gyanith- Technical Fest of NITPY", "category": "", "dept": "", "date": "27 FEB",
+         "link": "https://gyanith.org/",
+         "img": "/static/images/events/Gyanith_Technical_Fest.jpg", "alt": ""},
+        {"title": "ICWEES 2025 - International Conference on Water, Environment, Energy & Society",
+         "category": "Conference", "dept": "Department of Civil", "date": "26 APR",
+         "link": "https://icwees2025.com/",
+         "img": "/static/images/events/ICWEES_2025.jpeg", "alt": ""},
+        {"title": "Harnessing AI for Medical Signal and Imaging Diagnostics organized by department of ECE ...",
+         "category": "Workshop", "dept": "Department of ECE", "date": "09 DEC",
+         "link": "/static/pdf/Upcoming_Events/2024/12/ATAL_FDP_Brochure_2024.pdf",
+         "img": "/static/images/events/Harnessing_AI_Medical.jpg", "alt": ""},
+        {"title": "4th International Conference on Future Technologies in Manufacturing, ...",
+         "category": "Conference", "dept": "Department of Mechanical Engineering", "date": "11 DEC",
+         "link": "https://2024.icoftnitpy.com/",
+         "img": "/static/images/events/ICOFT_2024.png", "alt": ""},
+        {"title": "Five days short-term course on 'Trends in Shaping Energy Transition' through hybrid mode ...",
+         "category": "Workshop", "dept": "Department of EEE", "date": "25 NOV",
+         "link": "/static/pdf/Upcoming_Events/2024/11/STC_TSET_NITPY_Brochure.pdf",
+         "img": "/static/images/events/TSET_NITPY.png", "alt": ""},
+        {"title": "Decoding Chemistry Horizons: A Workshop for Aspiring Chemists (DCHWAC-2024)",
+         "category": "Workshop", "dept": "Department of Chemistry", "date": "",
+         "link": "/static/pdf/Upcoming_Events/2024/09/DCHWAC-2024.pdf",
+         "img": "/static/images/events/DCHWAC-2024.JPG", "alt": ""},
+        {"title": "IConSCEPT 2024 on 4 - 5 July, 2024 at National Institute of Technology Puducherry, Karaikal",
+         "category": "Workshop", "dept": "Department of EEE", "date": "04 JULY",
+         "link": "/static/pdf/Upcoming_Events/2024/04/IConscept_2024.pdf",
+         "img": "/static/images/events/IConSCEPT_2023.jpg", "alt": ""},
+        {"title": "Artificial Intelligence Models for Remote Sensing and its Applications (AIRS-2024)",
+         "category": "Workshop", "dept": "Department of CSE", "date": "06 JUNE",
+         "link": "https://prabakarankannan.github.io/airs2024/",
+         "img": "/static/images/events/AIRS_2024.jpg", "alt": ""},
+    ]
+    research_carousel_slides = [
+    {
+        "title": "Impact of online teaching on virtual laboratory course analysis using machine learning..",
+        "dept": "Mechanical",
+        "link": "https://pubs.aip.org/aip/acp/article-abstract/3217/1/020001/3327691/Impact-of-online-teaching-on-virtual-laboratory?redirectedFrom=fulltext",
+        "img": "images/instituteCarousel/research/Impact_of_online_teaching.jpg",
+
+        "alt": "",
+        "authors": ["Dr. Karpagaraj"]
+    },
+    {
+        "title": "Hot corrosion and high-temperature oxidation studies of hard-faced nickel alloy..",
+        "dept": "Mechanical",
+        "link": "https://shop.elsevier.com/books/advances-in-sustainable-materials/kumar/978-0-443-13849-2",
+        "img": "images/instituteCarousel/research/Advances_in_Sustainable_Materials.jpg",
+        "alt": "",
+        "authors": ["Dr. Karpagaraj"]
+    },
+    {
+        "title": "Optimized static configuration for output power maximization of..",
+        "dept": "Electrical and Electronics",
+        "link": "https://www.sciencedirect.com/science/article/abs/pii/S0306261924019810",
+        "img": "images/instituteCarousel/research/Applied_Energy.jpeg",
+        "alt": "",
+        "authors": ["Dr. BIJUKUMAR B"]
+    },
+    {
+        "title": "Cost-effective one-time configuration for bridge-linked thermoelectric ..",
+        "dept": "Electrical and Electronics",
+        "link": "https://www.sciencedirect.com/science/article/abs/pii/S0959652624022650",
+        "img": "images/instituteCarousel/research/Journal_of_Cleaner_Production.png",
+        "alt": "",
+        "authors": ["Dr. BIJUKUMAR B"]
+    },
+    {
+        "title": "A novel one-time unified configuration strategy with L-SHADE ..",
+        "dept": "Electrical and Electronics",
+        "link": "https://www.sciencedirect.com/science/article/abs/pii/S1359431124020441",
+        "img": "images/instituteCarousel/research/Applied_Thermal_Engineering.jpeg",
+        "alt": "",
+        "authors": ["Dr. BIJUKUMAR B"]
+    },
+    {
+        "title": "Effect of cold metal transfer process on hardfacing of Inconel 718 over ..",
+        "dept": "Mechanical",
+        "link": "https://www.sciencedirect.com/science/article/abs/pii/S0167577X24011698",
+        "img": "images/instituteCarousel/research/Effect_of_cold_metal_transfer.jpg",
+        "alt": "",
+        "authors": ["Dr. Karpagaraj"]
+    },
+    {
+        "title": "Book of Abstracts - International conference on i-Computer 2023 ...",
+        "dept": "CSE",
+        "link": "https://drive.google.com/file/d/1fVE_q0avKFvfmt4nUOIvAHmPKEf7eKds/view",
+        "img": "images/events/AIRS_2024.jpg",
+        "alt": "",
+        "authors": [""]
+    },
+    {
+        "title": "Phosphazene-Based Covalent Organic Framework as an Efficient Catalyst...",
+        "dept": "Chemistry",
+        "link": "https://pubs.acs.org/doi/10.1021/acsomega.3c08763",
+        "img": "images/instituteCarousel/research/COF.jpg",
+        "alt": "",
+        "authors": ["Dr. Seenuvasan Vedachalam"]
+    },
+    {
+        "title": "Experimental study on mechanical, damping and corrosion properties of Inconel..",
+        "dept": "Mechanical",
+        "link": "https://www.sciencedirect.com/science/article/abs/pii/S1350630723008257?via%3Dihub",
+        "img": "images/instituteCarousel/research/hard-faced_stainless_steel_304.jpg",
+        "alt": "",
+        "authors": ["Dr. Karpagaraj"]
+    },
+    {
+        "title": "Optimizing electric vehicle charging in distribution networks ...",
+        "dept": "Electrical and Electronics",
+        "link": "https://www.sciencedirect.com/science/article/abs/pii/S0360544224005875",
+        "img": "images/instituteCarousel/research/Optimizing_electric_vehicle_charging.jpg",
+        "alt": "",
+        "authors": ["Dr. T. Vinopraba"]
+    },
+    {
+        "title": "Dynamic pricing for load shifting: Reducing electric vehicle charging impacts",
+        "dept": "EEE",
+        "link": "https://www.sciencedirect.com/science/article/pii/S2210670724000854",
+        "img": "images/instituteCarousel/research/Dynamic_pricing_load_shifting.jpg",
+        "alt": "",
+        "authors": ["Dr. T. Vinopraba"]
+    },
+    {
+        "title": "Profuse Channel Estimation and Signal Detection Techniques for Orthogonal Time Frequency",
+        "dept": "ECE",
+        "link": "https://ieeexplore.ieee.org/document/10319451",
+        "img": "images/instituteCarousel/research/downlink.jpg",
+        "alt": "",
+        "authors": ["Dr. Surendar M"]
+    }
+]
+ 
+
 
     # Convert relative paths to Django static paths
     for slide in instituteCarouselSlides:
@@ -196,7 +330,9 @@ def home(request):
         "galleryImages": galleryImages,
         "nav_items": nav_items,
         "stats": stats,
-        "quickLinks": quickLinks
+        "quickLinks": quickLinks,
+        "research_carousel_slides": research_carousel_slides,
+        "eventsCarouselSlides": eventsCarouselSlides
     })
 
 def about(request):
